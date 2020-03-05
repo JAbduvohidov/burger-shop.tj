1ea80059a4045aa6443ec43202ec65a9bf352697
JAbduvohidov <jaga.vohidov@gmail.com>
2020-03-06 00:00:26 +0500

    fix: environment host and port

--- cmd/crud/main.go
+++ cmd/crud/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"burger-shop.tj/cmd/crud/app"
-	"burger-shop.tj/pkg/crud/services"
 	"burger-shop.tj/pkg/crud/services/burgers"
 	"context"
 	"flag"
@@ -20,35 +19,19 @@ const (
 )
 
 var (
-	hostF = flag.String("host", "", "Server host")
-	portF = flag.String("port", "", "Server port")
+	hostF = flag.String("host", "0.0.0.0", "Server host")
+	portF = flag.String("port", "9999", "Server port")
 	dsnF  = flag.String("dsn", "", "Postgres DSN")
 )
 
 func main() {
 	flag.Parse()
-	log.Println("host setting to connect")
-	host, ok := services.FlagOrEnv(hostF, ENV_HOST)
-	if !ok {
-		log.Panic("can't host setting")
-	}
-	log.Println("get port to connect")
-	port, ok := services.FlagOrEnv(portF, ENV_PORT)
-	if !ok {
-		log.Panic("can't port setting")
-	}
-	log.Println("set address to connect")
-	addr := net.JoinHostPort(host, port)
+	addr := net.JoinHostPort(*hostF, *portF)
 	log.Printf("address to connect: %s", addr)
 
-	log.Println("set database to connect")
-	dsn, ok := services.FlagOrEnv(dsnF, ENV_DSN)
-	if !ok {
-		log.Panic("How get DB url?")
-	}
-	log.Printf("try start server on: %s, dbUrl: %s", addr, dsn)
-	start(addr, dsn)
-	log.Printf("server success on: %s, dbUrl: %s", addr, dsn)
+	log.Printf("try start server on: %s, dbUrl: %s", addr, *dsnF)
+	start(addr, *dsnF)
+	log.Printf("server success on: %s, dbUrl: %s", addr, *dsnF)
 }
 
 func start(addr string, dsn string) {
