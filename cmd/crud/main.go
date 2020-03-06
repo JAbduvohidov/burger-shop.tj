9e9ffe29f0aa4310172fd491711f086ff10a14c4
JAbduvohidov <jaga.vohidov@gmail.com>
2020-03-06 09:27:01 +0500

    fix: port changed

--- cmd/crud/main.go
+++ cmd/crud/main.go
@@ -21,7 +21,7 @@ const (
 
 var (
 	hostF = flag.String("host", "0.0.0.0", "Server host")
-	portF = flag.String("port", "9999", "Server port")
+	portF = flag.String("port", "8080", "Server port")
 	dsnF  = flag.String("dsn", "postgres://nurzyxgxduryxt:a91147d43b56869a99a0815d324323f5f22071d6dfa17cdd789c93388a392072@ec2-52-86-73-86.compute-1.amazonaws.com:5432/dc1ns5rpr9g4e3", "Postgres DSN")
 )
 
